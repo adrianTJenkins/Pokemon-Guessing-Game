eb309d1c09770ec444d3b6acc9c07e26fe912f32
Adrian Jenkins <adrian.jenkins99@gmail.com>
2021-08-28 16:46:24 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/whosthatpokemonapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.whosthatpokemonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
